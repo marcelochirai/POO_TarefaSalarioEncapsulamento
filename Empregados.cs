695507902565cfd47a37d13a27f56c36a29caf36
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-10-28 23:19:38 -0300

    Add files via upload
    Crie um programa para ler um número inteiro N e depois os dados (id, nome, e salario) de N funcionários. Não deve repetição de id.
    Efetue o aumento de X por cento no salário de um determinado funcionário. Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionários, conforme exemplos.
    Lembre que a aplicação técnica de encapsulamento deve ser usada para não permitir que o salário possa ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de aumento por porcentagem dada.
    Utilize o UML disponível no conteúdo (slide 16)

---
+++ Empregados.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace POO_TarefaSalarioEncapsulamento
+{
+    class Empregados
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Empregados(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentoSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace POO_TarefaSalarioEncapsulamento
+//Programa para ler um número inteiro N e depois os dados (id,nome,esalario)de N funcionários. Não deve haver repetição de id.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de informações
+            Console.Write("Quantos funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Criação da lista Empregados
+            List<Empregados> list = new List<Empregados>();
+
+            //Criação do laço para inserção de dados conforme N
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Funcionário #" + i + ":");   //Informa o número sequencial do funcionário
+                Console.Write("Id: ");                          //Solicita um número de identificação "ID"
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");                        //Solicita o nome do funcionário
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");                     //Solicita o valor do salario inicial do funcionário
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                //Adição das informações na lista
+                list.Add(new Empregados(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.Write("ID do funcionário que receberá o aumento: ");    //Solicita o ID. 
+            int searchId = int.Parse(Console.ReadLine());                   //A variável searchId receberá o valor da ID informada.
+
+            //A variável emp receberá um valor da lista, caso seja encontrado o ID informado.
+            Empregados emp = list.Find(x => x.Id == searchId);
+            if (emp != null)    //Se a variável emp não for nula...
+            {
+                Console.Write("Porcentagem de aumento do salário: ");   //Solicita o valor percentual a ser adicionado.
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.AumentoSalario(porcentagem);
+            }
+            else  //Se o ID não existir...
+            {
+                Console.WriteLine("Este funcionário não existe!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada dos funcionários da Bravo:");
+
+            //Laço que cria uma lista de funcionários e seus salários.
+            foreach (Empregados obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
